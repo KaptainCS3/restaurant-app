0590f98b9a61b311fe02a5ca067f46f1e0a98d97
KaptainCS3 <mbiapplegryn@gmail.com>
2022-11-21 13:06:34 +0100

    renamed file

--- src/Data.js
+++ src/Data.js
@@ -1,6 +1,44 @@
-const foodList = {
-  "food-img": "",
-  "disk-name": "",
-  rating: 4,
-  "order-now": true,
-};
+export default [
+  {
+    "id": 1,
+    "dish-image": "./pexels-pixabay-56014.jpg",
+    "dish-name": "salade",
+    rating: 4,
+    "order-now": true,
+  },
+  {
+    "id": 2,
+    "dish-image": "./pexels-pixabay-56014.jpg",
+    "dish-name": "salade",
+    rating: 4,
+    "order-now": true,
+  },
+  {
+    "id": 3,
+    "dish-image": "./pexels-pixabay-56014.jpg",
+    "dish-name": "salade",
+    rating: 4,
+    "order-now": true,
+  },
+  {
+    "id": 4,
+    "dish-image": "./pexels-pixabay-56014.jpg",
+    "dish-name": "salade",
+    rating: 4,
+    "order-now": true,
+  },
+  {
+    "id": 5,
+    "dish-image": "./pexels-pixabay-56014.jpg",
+    "dish-name": "salade",
+    rating: 4,
+    "order-now": true,
+  },
+  {
+    "id": 6,
+    "dish-image": "./pexels-pixabay-56014.jpg",
+    "dish-name": "salade",
+    rating: 4,
+    "order-now": true,
+  },
+];
