e685bddc517de27b50c7e661e4a89a83de9e73b0
KaptainCS3 <mbiapplegryn@gmail.com>
2022-11-21 12:59:32 +0100

    stack component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import Nav from "./components/Nav";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Welcome from "./components/Main/Welcome";
+import About from "./components/Main/About";
+import Card from "./components/Main/Card";
+import data from "./data";
+import CardContainer from "./components/Main/CardContainer";
+
+const cardData = data.map((el) => {});
 const App = () => (
   <BrowserRouter>
     <Routes>
@@ -12,6 +18,10 @@ const App = () => (
           <div>
             <Nav />
             <Welcome />
+            <About />
+            <>
+            <CardContainer />
+            </>
             <Footer />
           </div>
         }
