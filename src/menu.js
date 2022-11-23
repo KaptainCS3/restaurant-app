1e2c381866f756a12de725c829453e100ac22c29
KaptainCS3 <mbiapplegryn@gmail.com>
2022-11-23 22:19:59 +0100

    data menu object

---
+++ src/menu.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    id: 1,
+    heading: "Main Course",
+    paragraph1: "Super-Delicious Zuppa Toscana",
+    small1: "Chicke . Italian . Sausage . Spinach",
+    paragraph2: "Bacon coil baked Australian beef",
+    small2: "Chicke . Italian . Sausage . Spinach",
+  },
+  {
+    id: 2,
+    heading: "Soups & Salads",
+    paragraph1: "Salat banana flower",
+    small1: "Chicke . Italian . Sausage . Spinach",
+    paragraph2: "Gourmet Mushroom Risotoo",
+    small2: "Chicke . Italian . Sausage . Spinach",
+  },
+  {
+    id: 3,
+    heading: "Desserts",
+    paragraph1: "Ice cream tarte apple",
+    small1: "Mushrooms . Chicken . Broth Arbario . Rice",
+    paragraph2: "Fruit 'Mille feuille'",
+    small2: "Mushrooms . Chicken . Broth Arbario . Rice",
+  },
+  {
+    id: 4,
+    heading: "Drinks",
+    paragraph1: "Double Chocolate Mud Pie",
+    small1: "Chicke . Italian . Sausage . Spinach",
+    paragraph2: "Pannacotta cream cake",
+    small2: "Chicke . Italian . Sausage . Spinach",
+  },
+];
