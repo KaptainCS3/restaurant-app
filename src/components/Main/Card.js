e568d03b1559fe0c78ab9f2d10d7bfa2045d474c
KaptainCS3 <mbiapplegryn@gmail.com>
2022-11-24 22:25:27 +0100

    update cards

--- src/components/Main/Card.js
+++ src/components/Main/Card.js
@@ -1,51 +1,32 @@
-import React from 'react'
-import style from "../../style/card.module.css"
-import data from "../../data.js";
+import React from "react";
+import style from "../../style/card.module.css";
+import star from "../../assets/rating_star.png";
 const Card = (props) => {
-    console.log(props)
   return (
-        <div className={style.food__container__list}>
-          <div className={style.food__content}>
-            <div className={style.dish__img}>
-                <img src={props.data} alt="content" />
-            </div>
-            <div className={style.food__description}>
-              <div className={style.name__rate}>
-                <p className={style.dish__name}></p>
-              </div>
-              <button className={style.order__now__btn}></button>
-            </div>
-          </div>
-          <div className={style.food__content}>
-            <div className={style.dish__img}></div>
-            <div className={style.food__description}>
-              <div className={style.name__rate}>
-                <p className={style.dish__name}></p>
-              </div>
-              <button className={style.order__now__btn}></button>
-            </div>
-          </div>
-          <div className={style.food__content}>
-            <div className={style.dish__img}></div>
-            <div className={style.food__description}>
-              <div className={style.name__rate}>
-                <p className={style.dish__name}></p>
-              </div>
-              <button className={style.order__now__btn}></button>
-            </div>
-          </div>
-          <div className={style.food__content}>
-            <div className={style.dish__img}></div>
-            <div className={style.food__description}>
-              <div className={style.name__rate}>
-                <p className={style.dish__name}></p>
-              </div>
-              <button className={style.order__now__btn}></button>
-            </div>
+    <div className={style.food__content}>
+      <div className={style.dish__img}>
+        <img
+          src={require(`../../assets/images/salade${props.dataPassed.dish_image}`)}
+          alt="content"
+          className={style.image__dish}
+        />
+      </div>
+      <div className={style.food__description}>
+        <div className={style.name__rate}>
+          <p className={style.dish__name}>{props.dataPassed.dish_name}</p>
+          <div className={style.rating__star}>
+            <img src={star} alt="description rating" />
+            <img src={star} alt="description rating" />
+            <img src={star} alt="description rating" />
+            <img src={star} alt="description rating" />
+            <img src={star} alt="description rating" />
+            <span>{props.dataPassed.rating}</span>
           </div>
         </div>
-    
+        <button className={style.order__now__btn}>&gt;</button>
+      </div>
+    </div>
   );
-}
+};
 
-export default Card
+export default Card;
