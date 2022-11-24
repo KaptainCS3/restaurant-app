734f9230dff421fe83bca334f4502b582a1fc325
KaptainCS3 <mbiapplegryn@gmail.com>
2022-11-24 22:30:12 +0100

    dish menu

---
+++ src/components/Main/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import style from '../../style/menu.module.css';
+const Menu = (props) => {
+  return (
+    <div className={style.menu__item}>
+      <h4 className={style.discover__sub}>{props.element.heading}</h4>
+      <p className={style.para__1}>{props.element.paragraph1}</p>
+      <small className={style.small__1}>{props.element.small1}</small>
+      <p className={style.para__2}>{props.element.paragraph2}</p>
+      <small className={style.small__2}>{props.element.small2}</small>
+    </div>
+  );
+}
+
+export default Menu
