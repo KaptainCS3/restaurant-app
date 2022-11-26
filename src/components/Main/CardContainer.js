fa1ab88cc13299eabf247b61ed0afdc95c8d0e45
KaptainCS3 <mbiapplegryn@gmail.com>
2022-11-26 22:26:27 +0100

    update component

--- src/components/Main/CardContainer.js
+++ src/components/Main/CardContainer.js
@@ -1,20 +1,24 @@
-import React from 'react'
-import style from "../../style/cardcontainer.module.css"
-import Card from './Card';
+import React from "react";
+import style from "../../style/cardcontainer.module.css";
+import Card from "./Card";
+import data from "../../data";
+import Paging from "./Paging";
 const CardContainer = () => {
+  const cardData = data.map((el) => {
+    return <Card key={el.id} dataPassed={el} />;
+  });
   return (
     <section className={style.feature__food}>
       <h3 className={style.explore}>Explore</h3>
       <div className={style.food__container}>
         <h2 className={style.food__heading}>Our Featured Food</h2>
-        <div className={style.food__paging}>
-          <div className={style.list__left}>&lt;</div>
-          <div className={style.list__right}>&gt;</div>
-        </div>
+        <Paging />
       </div>
-      <Card />
+      <div className={style.food__container__list}>
+      {cardData}
+      </div>
     </section>
   );
-}
+};
 
-export default CardContainer
+export default CardContainer;
