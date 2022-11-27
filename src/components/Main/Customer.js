29852be53ccb35307c7b5595ca915107da3ed7d2
KaptainCS3 <mbiapplegryn@gmail.com>
2022-11-27 20:59:05 +0100

    update component

--- src/components/Main/Customer.js
+++ src/components/Main/Customer.js
@@ -6,8 +6,8 @@ import style from "../../style/customer.module.css"
 const Customer = () => {
   return (
     <section className={style.customer__container}>
-        <h3 className={style.customer__heading}></h3>
-        <h2 className={style.customer__main}></h2>
+        <h3 className={style.customer__heading}>Some</h3>
+        <h2 className={style.customer__main}>Our Happy Customers</h2>
         <CustomerCard />
       <Paging />
     </section>
