4991981bb6910ac4880cfdc46c29e6fa56418b99
KaptainCS3 <mbiapplegryn@gmail.com>
2022-11-28 21:24:30 +0100

    initial commit

---
+++ src/components/Main/TabLinks.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import style from "../../style/galleryMenu.module.css";
+const TabLinks = (props) => {
+  console.log(props);
+  return <Link className={style.gallery__links}>{props.el.links}</Link>;
+};
+
+export default TabLinks;
