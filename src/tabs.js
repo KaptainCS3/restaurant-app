c9b5d952057ee7993d5a1af7ac788d5879a320a3
KaptainCS3 <mbiapplegryn@gmail.com>
2022-11-28 21:26:10 +0100

    initial commit

---
+++ src/tabs.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    links: "Breakfast",
+  },
+  {
+    id: 2,
+    links: "Salads",
+  },
+  {
+    id: 3,
+    links: "Meat & Fish",
+  },
+  {
+    id: 4,
+    links: "Soups",
+  },
+  {
+    id: 5,
+    links: "Desserts",
+  },
+  {
+    id: 6,
+    links: "Drinks",
+  },
+];
