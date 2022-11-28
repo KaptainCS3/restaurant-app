a6b1128d31cc33c5da481dfc70e5a2bb648fa306
KaptainCS3 <mbiapplegryn@gmail.com>
2022-11-28 21:24:45 +0100

    update component

--- src/components/Main/GalleryCard.js
+++ src/components/Main/GalleryCard.js
@@ -5,18 +5,7 @@ import variable from "../../assets/images/pizza/pexels-kevin-bidwell-1788852.jpg
 const GalleryCard = () => {
   return (
     <div className={style.gallary__card}>
-        <div>
-          <img src={variable} alt="description" />
-        </div>
-        <div>
-          <img src={variable} alt="description" />
-        </div>
-        <div>
-          <img src={variable} alt="description" />
-        </div>
-        <div>
-          <img src={variable} alt="description" />
-        </div>
+        
     </div>
   )
 }
