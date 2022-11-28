ec329d2db01601e7d271fa20cc80594754eaa90b
KaptainCS3 <mbiapplegryn@gmail.com>
2022-11-28 21:25:43 +0100

    update component

--- src/components/Main/CustomerCard.js
+++ src/components/Main/CustomerCard.js
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import style from "../../style/customerCard.module.css"
 const CustomerCard = () => {
   return (
-    <div>CustomerCard</div>
+    <div className={style.card__customer}>
+        <div className={style.avarta__rate}>
+            {/* <img src={} alt="customer avarta"/> */}
+        </div>
+        <p className={style.description}>
+            
+        </p>
+        <div className={style.rating__name}>
+            <div className={style.rating}></div>
+            <h5 className={style.avarta__name}></h5>
+        </div>
+    </div>
   )
 }
 
