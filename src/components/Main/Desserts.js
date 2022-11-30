afc53febca0968b3c2ee019df1a5b2e5d22f434c
KaptainCS3 <mbiapplegryn@gmail.com>
2022-11-30 23:36:38 +0100

    render component img

--- src/components/Main/Desserts.js
+++ src/components/Main/Desserts.js
@@ -1,12 +1,12 @@
-import React from 'react'
-
+import React from "react";
+import style from "../../style/desserts.module.css"
 const Desserts = (props) => {
-  console.log(props.data)
-  return (
-    <div>
+  const desserts = props.data.images.map((el) => {
+    return <img src={require(`../../assets/images/desserts/pexels-alexander-grey-1191639.jpg`)} alt="image-description" />
+  });
+  return <div className={style.div}>
+    {desserts}
+    </div>;
+};
 
-    </div>
-  )
-}
-
-export default Desserts
+export default Desserts;
