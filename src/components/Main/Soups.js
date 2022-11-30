7706a6cbd41db446d37da05ad0da60d0bedc5267
KaptainCS3 <mbiapplegryn@gmail.com>
2022-11-30 23:37:49 +0100

    render component img

--- src/components/Main/Soups.js
+++ src/components/Main/Soups.js
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import style from "../../style/desserts.module.css";
 const Soups = (props) => {
-  console.log(props.data);
-  return (
-    <div>Soups</div>
-  )
+  const salad = props.data.images.map((el) => {
+    return (
+      <img
+        src={require(`../../assets/images/desserts/pexels-alexander-grey-1191639.jpg`)}
+        alt="image-description"
+      />
+    );
+  });
+  return <div className={style.div}>{salad}</div>;
 }
 
 export default Soups
