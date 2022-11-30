1829796ddc8777ab9a9b0f9d14826c69cc86ad39
KaptainCS3 <mbiapplegryn@gmail.com>
2022-11-30 23:36:33 +0100

    render component img

--- src/components/Main/BreakFast.js
+++ src/components/Main/BreakFast.js
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import style from "../../style/desserts.module.css";
 const BreakFast = (props) => {
-   console.log(props.data)
-  return (
-    <div>BreakFast</div>
-  )
-}
+  const salad = props.data.images.map((el) => {
+    return (
+      <img
+        src={require(`../../assets/images/breakfast/pexels-gerald-jake-abangan-2689421.jpg`)}
+        alt="image-description"
+      />
+    );
+  });
+  return <div className={style.div}>{salad}</div>;
+};
 
 export default BreakFast
