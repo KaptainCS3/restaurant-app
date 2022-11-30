e2b45772308b5ee2bcc6be17581276e7b95a3301
KaptainCS3 <mbiapplegryn@gmail.com>
2022-11-30 23:37:37 +0100

    render component img

--- src/components/Main/MeatFish.js
+++ src/components/Main/MeatFish.js
@@ -1,12 +1,15 @@
 import React from 'react'
-
+import style from "../../style/desserts.module.css";
 const MeatFish = (props) => {
-  console.log(props.data);
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const salad = props.data.images.map((el) => {
+    return (
+      <img
+        src={require(`../../assets/images/fish_meat/pexels-airam-datoon-8792899.jpg`)}
+        alt="image-description"
+      />
+    );
+  });
+  return <div className={style.div}>{salad}</div>;
+};
 
 export default MeatFish
