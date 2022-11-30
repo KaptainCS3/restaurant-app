dd18e169eeb52c998820a5dd8edab264f04f01af
KaptainCS3 <mbiapplegryn@gmail.com>
2022-11-30 23:37:57 +0100

    minor changes

--- src/components/Main/TabLinks.js
+++ src/components/Main/TabLinks.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import style from "../../style/galleryMenu.module.css";
 const TabLinks = (props) => {
-  console.log(props);
   return <Link className={style.gallery__links}>{props.el.links}</Link>;
 };
 
