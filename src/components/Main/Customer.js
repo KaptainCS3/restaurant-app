da1ec00a727487b17f3db4fe52a297cee23f0411
KaptainCS3 <mbiapplegryn@gmail.com>
2022-12-01 22:02:52 +0100

    update component

--- src/components/Main/Customer.js
+++ src/components/Main/Customer.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import CustomerCard from './CustomerCard'
-import Paging from './Paging'
-import style from "../../style/customer.module.css"
+import React from "react";
+import CustomerCard from "./CustomerCard";
+import Paging from "./Paging";
+import style from "../../style/customer.module.css";
 
 const Customer = () => {
   return (
     <section className={style.customer__container}>
-        <h3 className={style.customer__heading}>Some</h3>
-        <h2 className={style.customer__main}>Our Happy Customers</h2>
+      <h3 className={style.customer__heading}>Some</h3>
+      <h2 className={style.customer__main}>Our Happy Customers</h2>
+      <div className={style.div__el}>
         <CustomerCard />
+        <CustomerCard />
+        <CustomerCard />
+      </div>
       <Paging />
     </section>
-  )
-}
+  );
+};
 
-export default Customer
+export default Customer;
