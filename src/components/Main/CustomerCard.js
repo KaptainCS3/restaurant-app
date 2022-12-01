ed89099da323ef40fcaa943af353132d3f9bdd4e
KaptainCS3 <mbiapplegryn@gmail.com>
2022-12-01 22:02:59 +0100

    update component

--- src/components/Main/CustomerCard.js
+++ src/components/Main/CustomerCard.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import style from "../../style/customerCard.module.css"
+import avarta_1 from "../../assets/avartas/pexels-daniel-xavier-1239291.jpg"
+import avarta_2 from "../../assets/avartas/pexels-pixabay-220453.jpg"
+import avarta_3 from "../../assets/avartas/pexels-stefan-stefancik-91227.jpg"
 const CustomerCard = () => {
   return (
     <div className={style.card__customer}>
-        <div className={style.avarta__rate}>
-            {/* <img src={} alt="customer avarta"/> */}
-        </div>
-        <p className={style.description}>
-            
-        </p>
-        <div className={style.rating__name}>
-            <div className={style.rating}></div>
-            <h5 className={style.avarta__name}></h5>
-        </div>
+      <div className={style.avarta__rate}>
+        <img src={avarta_1} alt="pexel" />
+        <div className={style.rating}>adfafdslfsidf</div>
+      </div>
+      <p className={style.description}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+        voluptate nemo porro voluptas tempore. Est incidunt doloribus ea esse,
+        alias excepturi beatae dolore laboriosam modi quam aperiam aliquam quia
+        temporibus?.
+      </p>
     </div>
-  )
+  );
 }
 
 export default CustomerCard
