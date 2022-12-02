e1455863a792bec68c112c5b99048d1f3eb8eeaa
KaptainCS3 <mbiapplegryn@gmail.com>
2022-12-02 23:26:29 +0100

    initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import list from '../../list'
+const Footer = () => {
+  console.log(list[0].breakfast.meal)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Footer
