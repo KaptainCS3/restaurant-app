14841866e8cb3ebf5dab495b3eb31ae945b25ded
KaptainCS3 <mbiapplegryn@gmail.com>
2022-12-03 23:36:09 +0100

    update component

--- src/components/Main/BookMeal.js
+++ src/components/Main/BookMeal.js
@@ -3,15 +3,16 @@ import style from "../../style/book.module.css"
 const Book = () => {
   return (
     <section className={style.book}>
-      <h3></h3>
-      <h2></h2>
-      <form>
+      <h3>Let's </h3>
+      <h2>Book a Table</h2>
+      <form className={style.form__container}>
         <label htmlFor="date">Date</label>
-        <input type="date" id="date"/>
+        <input type="date" id="date" className={style.form__input}/>
         <label htmlFor="time">Time</label>
-        <input type="time" id="time"/>
+        <input type="time" id="time" className={style.form__input}/>
         <label htmlFor="guest">Guest</label>
-        <input type="text" id="guest"/>
+        <input type="text" id="guest" className={style.form__input}/>
+        <input type="email" placeholder="Enter your email" id="email"  className={style.form__input}/>
       </form>
       <button className={style.available__btn}></button>
     </section>
