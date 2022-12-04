2f1427e64c312d7a0a7c3c4922dbf2907f72b475
KaptainCS3 <mbiapplegryn@gmail.com>
2022-12-04 18:22:13 +0100

    minor changes

--- src/components/Main/BookMeal.js
+++ src/components/Main/BookMeal.js
@@ -1,20 +1,33 @@
 import React from "react";
-import style from "../../style/book.module.css"
+import style from "../../style/book.module.css";
 const Book = () => {
   return (
     <section className={style.book}>
-      <h3>Let's </h3>
-      <h2>Book a Table</h2>
+      <h3 className={style.heading}>Let's </h3>
+      <h2 className={style.sub__headingx}>Book a Table</h2>
       <form className={style.form__container}>
-        <label htmlFor="date">Date</label>
-        <input type="date" id="date" className={style.form__input}/>
-        <label htmlFor="time">Time</label>
-        <input type="time" id="time" className={style.form__input}/>
-        <label htmlFor="guest">Guest</label>
-        <input type="text" id="guest" className={style.form__input}/>
-        <input type="email" placeholder="Enter your email" id="email"  className={style.form__input}/>
+        <div className={style.form__control}>
+          <label htmlFor="date">Date</label>
+          <input type="date" id="date" className={style.form__input} />
+        </div>
+        <div className={style.form__control}>
+          <label htmlFor="time">Time</label>
+          <input type="time" id="time" className={style.form__input} />
+        </div>
+        <div className={style.form__control}>
+          <label htmlFor="guest">Guest</label>
+          <input type="text" id="guest" className={style.form__input} />
+        </div>
+        <div className={style.form__control}>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            id="email"
+            className={style.form__input}
+          />
+        </div>
       </form>
-      <button className={style.available__btn}></button>
+      <button type="submit" className={style.available__btn}>Check Availability &gt;</button>
     </section>
   );
 };
