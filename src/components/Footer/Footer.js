74442a84bcf6d8ce334266cf8178297eefb62f00
KaptainCS3 <mbiapplegryn@gmail.com>
2022-12-05 23:20:36 +0100

    minor changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,56 @@
-import React from 'react'
-import list from '../../list'
+import React from "react";
+import style from "../../style/footer.module.css";
 const Footer = () => {
-  console.log(list[0].breakfast.meal)
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer className={style.footer__container}>
+      <h1 className={style.hero__footer}>
+        <span>The</span> Kapstro
+      </h1>
+      <section className={style.footer__main}>
+        <div className={style.about}>
+          <h2 className={style.sub__heading}>About us</h2>
+          <p className={style.description}>Sed ut perspicialis unde omnis iste natus error sit vluptatem accusan0tium doloremque laudantium, totam rem aperiam,</p>
+        </div>
+        <div className={style.subscribe}>
+          <h2 className={style.sub__heading}>Get News & Offers</h2>
+          <form className={style.submit__offer}>
+            <input className={style.email} placeholder="Enter your email" id="email_offers"/>
+            <button className={style.btn__submit}></button>
+          </form>
+        </div>
+        <div className={style.contact}>
+          <h2 className={style.sub__heading}>Contact For Offers</h2>
+          <div className={style.locations}>
+            <p className={style.content}>
+              677 Main Drive
+              <br />
+              North Canton, OH 44720
+            </p>
+            <p className={style.content}>
+              +00 123 456 7890
+              <br />
+              www.Kapstro.com
+            </p>
+            <p className={style.content}>
+              Monday - Wednesday
+              <br />
+              7:00 - 9:00PM
+            </p>
+            <p className={style.content}>
+              Wednesday - Sunday
+              <br />
+              8:00 - 10:30PM
+            </p>
+          </div>
+        </div>
+      </section>
+      <div className={style.copyright}>
+        <p className={style.copyright__text}>
+          Copyright Reserve 2022 KaptainCS3
+        </p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
