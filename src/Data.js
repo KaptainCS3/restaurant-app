b0c24299c54bc6f139fde2650a2abb82ca564a51
KaptainCS3 <mbiapplegryn@gmail.com>
2022-12-09 23:29:47 +0100

    minor changes

--- src/Data.js
+++ src/Data.js
@@ -1,44 +1,49 @@
+
 export default [
   {
-    "id": 1,
-    "dish-image": "./pexels-pixabay-56014.jpg",
-    "dish-name": "salade",
+    id: 1,
+    "dish_image": "/pexels-pixabay-56014.jpg",
+    "dish_name": "salad",
+    // "rating_stars": "./"
     rating: 4,
-    "order-now": true,
+    order_now: true,
   },
   {
-    "id": 2,
-    "dish-image": "./pexels-pixabay-56014.jpg",
-    "dish-name": "salade",
+    id: 2,
+    "dish_image": "/pexels-julie-aagaard-2097090.jpg",
+    "dish_name": "salad",
+    // "rating_stars": "./"
     rating: 4,
-    "order-now": true,
+    order_now: true,
   },
   {
-    "id": 3,
-    "dish-image": "./pexels-pixabay-56014.jpg",
-    "dish-name": "salade",
+    id: 3,
+    dish_image: "/pexels-cats-coming-406152.jpg",
+    dish_name: "salad",
+    // "rating_stars": "./"
     rating: 4,
-    "order-now": true,
+    order_now: true,
   },
   {
-    "id": 4,
-    "dish-image": "./pexels-pixabay-56014.jpg",
-    "dish-name": "salade",
+    id: 4,
+    dish_image: "/pexels-iina-luoto-1211887.jpg",
+    dish_name: "salad",
+    // "rating_stars": "./"
     rating: 4,
-    "order-now": true,
+    order_now: true,
   },
-  {
-    "id": 5,
-    "dish-image": "./pexels-pixabay-56014.jpg",
-    "dish-name": "salade",
-    rating: 4,
-    "order-now": true,
-  },
-  {
-    "id": 6,
-    "dish-image": "./pexels-pixabay-56014.jpg",
-    "dish-name": "salade",
-    rating: 4,
-    "order-now": true,
-  },
+  // {
+  //   id: 5,
+  //   dish_image: "/pexels-iina-luoto-1211887.jpg",
+  //   dish_name: "salade",
+  //   rating: 4,
+  //   "order-now": true,
+  // },
+  // {
+  //   id: 6,
+  //   dish_image: "/pexels-iina-luoto-1211887.jpg",
+  //   dish_name: "salade",
+  //   rating: 4,
+  //   "order-now": true,
+  // },
 ];
