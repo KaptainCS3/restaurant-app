df7454fd7cfe27c7f5e17951cb7c841692f45db3
KaptainCS3 <mbiapplegryn@gmail.com>
2022-12-10 22:39:25 +0100

    minor changes

--- src/components/Main/MeatFish.js
+++ src/components/Main/MeatFish.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import style from "../../style/desserts.module.css";
-const MeatFish = (props) => {
-  const salad = props.data.images.map((el) => {
-    return (
-      <img
-        src={require(`../../assets/images/fish_meat/pexels-airam-datoon-8792899.jpg`)}
-        alt="image-description"
-      />
-    );
-  });
-  return <div className={style.div}>{salad}</div>;
-};
+// import React from 'react'
+// import style from "../../style/desserts.module.css";
+// const MeatFish = (props) => {
+//   const salad = props.data.images.map((el) => {
+//     return (
+//       <img
+//         src={require(`../../assets/images/fish_meat/pexels-airam-datoon-8792899.jpg`)}
+//         alt="image-description"
+//       />
+//     );
+//   });
+//   return <div className={style.div}>{salad}</div>;
+// };
 
-export default MeatFish
+// export default MeatFish
