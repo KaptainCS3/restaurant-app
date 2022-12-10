2105a3e2a6319ebe5441061cfdd9d532c2bf61e1
KaptainCS3 <mbiapplegryn@gmail.com>
2022-12-10 22:39:39 +0100

    minor changes

--- src/components/Main/BreakFast.js
+++ src/components/Main/BreakFast.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import style from "../../style/desserts.module.css";
-const BreakFast = (props) => {
-  const salad = props.data.images.map((el) => {
-    return (
-      <img
-        src={require(`../../assets/images/breakfast/pexels-gerald-jake-abangan-2689421.jpg`)}
-        alt="image-description"
-      />
-    );
-  });
-  return <div className={style.div}>{salad}</div>;
-};
+// import React from 'react'
+// import style from "../../style/desserts.module.css";
+// const BreakFast = (props) => {
+//   const salad = props.data.images.map((el) => {
+//     return (
+//       <img
+//         src={require(`../../assets/images/breakfast/pexels-gerald-jake-abangan-2689421.jpg`)}
+//         alt="image-description"
+//       />
+//     );
+//   });
+//   return <div className={style.div}>{salad}</div>;
+// };
 
-export default BreakFast
+// export default BreakFast
