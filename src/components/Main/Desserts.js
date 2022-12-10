a7144d9e8f73f64cdad7353e0b331415129008d4
KaptainCS3 <mbiapplegryn@gmail.com>
2022-12-10 22:39:30 +0100

    minor changes

--- src/components/Main/Desserts.js
+++ src/components/Main/Desserts.js
@@ -1,12 +1,12 @@
-import React from "react";
-import style from "../../style/desserts.module.css"
-const Desserts = (props) => {
-  const desserts = props.data.images.map((el) => {
-    return <img src={require(`../../assets/images/desserts/pexels-alexander-grey-1191639.jpg`)} alt="image-description" />
-  });
-  return <div className={style.div}>
-    {desserts}
-    </div>;
-};
+// import React from "react";
+// import style from "../../style/desserts.module.css"
+// const Desserts = (props) => {
+//   const desserts = props.data.images.map((el) => {
+//     return <img src={require(`../../assets/images/desserts/pexels-alexander-grey-1191639.jpg`)} alt="image-description" />
+//   });
+//   return <div className={style.div}>
+//     {desserts}
+//     </div>;
+// };
 
-export default Desserts;
+// export default Desserts;
