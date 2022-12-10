1f0ea6520260f2021d5d37c7ac046ab4fabb6b3e
KaptainCS3 <mbiapplegryn@gmail.com>
2022-12-10 22:39:12 +0100

    minor changes

--- src/components/Main/Salads.js
+++ src/components/Main/Salads.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import style from "../../style/desserts.module.css";
-const Salads = (props) => {
-  const salad = props.data.images.map((el) => {
-    return <img src={require(`../../assets/images/salade/pexels-polina-tankilevitch-4519053.jpg`)} alt="image-description" />
-  });
-  return <div className={style.div}>{salad}</div>;
-};
+// import React from 'react'
+// import style from "../../style/desserts.module.css";
+// const Salads = (props) => {
+//   const salad = props.data.images.map((el) => {
+//     return <img src={require(`../../assets/images/salade/pexels-polina-tankilevitch-4519053.jpg`)} alt="image-description" />
+//   });
+//   return <div className={style.div}>{salad}</div>;
+// };
 
-export default Salads
+// export default Salads
