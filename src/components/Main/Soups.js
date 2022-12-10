cb9b39405dc5bc6889a6062caa1a23b94b183c2c
KaptainCS3 <mbiapplegryn@gmail.com>
2022-12-10 22:39:06 +0100

    minor changes

--- src/components/Main/Soups.js
+++ src/components/Main/Soups.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import style from "../../style/desserts.module.css";
-const Soups = (props) => {
-  const salad = props.data.images.map((el) => {
-    return (
-      <img
-        src={require(`../../assets/images/desserts/pexels-alexander-grey-1191639.jpg`)}
-        alt="image-description"
-      />
-    );
-  });
-  return <div className={style.div}>{salad}</div>;
-}
+// import React from 'react'
+// import style from "../../style/desserts.module.css";
+// const Soups = (props) => {
+//   const salad = props.data.images.map((el) => {
+//     return (
+//       <img
+//         src={require(`../../assets/images/desserts/pexels-alexander-grey-1191639.jpg`)}
+//         alt="image-description"
+//       />
+//     );
+//   });
+//   return <div className={style.div}>{salad}</div>;
+// }
 
-export default Soups
+// export default Soups
