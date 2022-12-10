f05ef08819d53eb604747d837ba797523372b760
KaptainCS3 <mbiapplegryn@gmail.com>
2022-12-10 22:39:19 +0100

    minor changes

--- src/components/Main/Drinks.js
+++ src/components/Main/Drinks.js
@@ -1,15 +1,15 @@
-import React from "react";
-import style from "../../style/desserts.module.css";
-const Drinks = (props) => {
-  const salad = props.data.images.map((el) => {
-    return (
-      <img
-        src={require(`../../assets/images/drinks/pexels-marta-dzedyshko-2775860.jpg`)}
-        alt="image-description"
-      />
-    );
-  });
-  return <div className={style.div}>{salad}</div>;
-};
+// import React from "react";
+// import style from "../../style/desserts.module.css";
+// const Drinks = (props) => {
+//   const salad = props.data.images.map((el) => {
+//     return (
+//       <img
+//         src={require(`../../assets/images/drinks/pexels-marta-dzedyshko-2775860.jpg`)}
+//         alt="image-description"
+//       />
+//     );
+//   });
+//   return <div className={style.div}>{salad}</div>;
+// };
 
-export default Drinks;
+// export default Drinks;
