89610a47b145f22348ecc03db3863202bceb9064
KaptainCS3 <mbiapplegryn@gmail.com>
2022-12-17 20:01:49 +0100

    initial commit

---
+++ src/components/Main/Discover.js
@@ -0,0 +1,18 @@
+import React from "react";
+import style from "../../style/discover.module.css";
+import menu from "../../menu";
+import Menu from "../Main/Menu";
+const Discover = () => {
+  const menuData = menu.map((el) => {
+    return <Menu key={el.id} element={el} />;
+  });
+  return (
+    <section className={style.discover__container}>
+      <h3 className={style.discover__heading}>Discover</h3>
+      <h2 className={style.discover__main}>Our Food Menu</h2>
+      <div className={style.discover__menu}>{menuData}</div>
+    </section>
+  );
+};
+
+export default Discover;
