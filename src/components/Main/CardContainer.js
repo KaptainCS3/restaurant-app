bea5ed8f42fecbefb70170c8f24bf690c886268f
KaptainCS3 <mbiapplegryn@gmail.com>
2022-12-18 23:47:10 +0100

    passing props to sample cards

--- src/components/Main/CardContainer.js
+++ src/components/Main/CardContainer.js
@@ -1,24 +1,23 @@
 import React from "react";
 import style from "../../style/cardcontainer.module.css";
 import Card from "./Card";
-import data from "../../data";
 import Paging from "./Paging";
 const CardContainer = () => {
-  const cardData = data.map((el) => {
-    return <Card key={el.id} dataPassed={el} />;
-  });
   return (
     <section className={style.feature__food}>
       <div className={style.wrapper}>
-      <h3 className={style.explore}>Explore</h3>
-      <div className={style.food__container}>
-        <h2 className={style.food__heading}>Our Featured Food</h2>
-        <Paging />
+        <h3 className={style.explore}>Explore</h3>
+        <div className={style.food__container}>
+          <h2 className={style.food__heading}>Our Featured Food</h2>
+          <Paging />
+        </div>
       </div>
+      <div className="flex flex-row justify-between" style={{ padding: "0 5%" }}>
+      <Card img="pexels-nishant-aneja-2955819.jpg"/>
+      <Card img="pexels-rajesh-tp-1633578.jpg"/>
+      {/* <Card img="pexels-steve-3789885.jpg"/> */}
       </div>
-      <div className={style.food__container__list}>
-      {cardData}
-      </div>
+      {/* <Card img="pexels-rajesh-tp-1633578.jpg"/> */}
     </section>
   );
 };
