2e8323ab6d7b0479438ca803d34a8530e4a12b89
KaptainCS3 <mbiapplegryn@gmail.com>
2022-12-19 21:46:23 +0100

    minor changes

--- src/components/Main/Paging.js
+++ src/components/Main/Paging.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import style from "../../style/cardcontainer.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 const Paging = () => {
   return (
     <div className={style.food__paging}>
-      <div className={style.list__left}>&lt;</div>
-      <div className={style.list__right}>&gt;</div>
+      <div className={style.list__left}><FontAwesomeIcon icon={faArrowLeft} /></div>
+      <div className={style.list__right}><FontAwesomeIcon icon={faArrowRight}/> </div>
     </div>
   );
 }
