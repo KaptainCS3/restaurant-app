beceacd339313c8670fb419f45a04a99b628c934
KaptainCS3 <mbiapplegryn@gmail.com>
2022-12-20 16:12:56 +0100

    remove react import

--- src/components/Main/Paging.js
+++ src/components/Main/Paging.js
@@ -1,9 +1,9 @@
-import React from 'react'
 import style from "../../style/cardcontainer.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 const Paging = () => {
   return (
+    //! paging arrow component
     <div className={style.food__paging}>
       <div className={style.list__left}><FontAwesomeIcon icon={faArrowLeft} /></div>
       <div className={style.list__right}><FontAwesomeIcon icon={faArrowRight}/> </div>
--- src/components/Main/TabLinks.js
+++ src/components/Main/TabLinks.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import style from "../../style/galleryMenu.module.css";
 const TabLinks = (props) => {
