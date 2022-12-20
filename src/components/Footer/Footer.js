0f46f30d3f08be980151bd3406779a63a99a444d
KaptainCS3 <mbiapplegryn@gmail.com>
2022-12-20 16:56:57 +0100

    form valid and store states

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,19 @@ import style from "../../style/footer.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 const Footer = () => {
+
+  //! declare and initialize states
   const [newsMail, setNewsMail] = useState({
     emailSub: "",
   });
   console.log(newsMail);
+
+  //! prevent submit page refresh
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  //! handle input change function
   const handleChange = (event) => {
     console.log(event);
     const { name, value } = event.target;
@@ -15,6 +24,25 @@ const Footer = () => {
       [name]: value,
     }));
   };
+  //! declare input node allias
+  const email = document.querySelector("#email_offers")
+  const errorMessage = document.querySelector("#error__message")
+
+  const handleClick = () =>{
+    alert("hiiiiiiiiiiiiiiiiii!")
+    if (!email.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+      email.style.borderColor = "#f96262";
+      errorMessage.innerHTML = "Please check your email";
+      return false;
+    } else {
+      email.style.borderColor = "#0f0";
+      errorMessage.innerHTML = "";
+      handleSubmit();
+      return true;
+    }
+  }
+
+
   return (
     <footer className={style.footer__container}>
       <h1 className={style.hero__footer}>
@@ -40,7 +68,8 @@ const Footer = () => {
               name="emailSub"
               value={newsMail.emailSub}
             />
-            <button className={style.btn__submit}>
+            <span className={style.error__message} id="error__message"></span>
+            <button className={style.btn__submit} onClick={handleClick} onSubmit={handleSubmit}>
               <FontAwesomeIcon icon={faArrowRight} style={{ color: "#fff" }} />
             </button>
           </form>
