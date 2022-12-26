672faf01e2750dc9a66b0eb408096b49685a66cc
Leonard Appelgryn <70280902+KaptainCS3@users.noreply.github.com>
2022-12-26 21:45:47 +0100

    Import tablink component

--- src/components/Main/GalleryCard.js
+++ src/components/Main/GalleryCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import style from "../../style/gallaryCard.module.css"
-import list from '../../list'
+import list from '../../list.json'
+import TabLink from "./TabLink"
 import variable from "../../assets/images/pizza/pexels-kevin-bidwell-1788852.jpg"
 const GalleryCard = () => {
   return (
