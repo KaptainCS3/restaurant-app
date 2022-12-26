6b67968bf6fdc2e1560a08d1cf01895f01fe32ee
Leonard Appelgryn <70280902+KaptainCS3@users.noreply.github.com>
2022-12-26 21:31:46 +0100

    Map and return value from tabs object

--- src/components/Main/TabLinks.js
+++ src/components/Main/TabLinks.js
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
 import style from "../../style/galleryMenu.module.css";
+import tabs from "../../tabs.js"
 const TabLinks = (props) => {
-  return <Link className={style.gallery__links}>{props.el.links}</Link>;
+  const mapTabs = tabs.map(el =>{
+   return <Link className={style.gallery__links}>{el.links}</Link>
+  })
+  return {mapTabs}
 };
 
 export default TabLinks;
