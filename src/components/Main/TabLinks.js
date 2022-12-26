e4644a79f8ca19a6d9592c358bc94bb888964485
Leonard Appelgryn <70280902+KaptainCS3@users.noreply.github.com>
2022-12-26 21:33:05 +0100

    Update TabLinks.js

--- src/components/Main/TabLinks.js
+++ src/components/Main/TabLinks.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import style from "../../style/galleryMenu.module.css";
-import tabs from "../../tabs.js"
+import tabs from "../../tabs"
 const TabLinks = (props) => {
   const mapTabs = tabs.map(el =>{
    return <Link className={style.gallery__links}>{el.links}</Link>
