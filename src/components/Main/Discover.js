9c11fba5a526673dea3563c027f6ce31aca15116
KaptainCS3 <mbiapplegryn@gmail.com>
2023-01-06 19:45:19 +0100

    add animations

--- src/components/Main/Discover.js
+++ src/components/Main/Discover.js
@@ -1,4 +1,5 @@
 import style from "../../style/discover.module.css";
+import Bounce from "react-reveal";
 import menu from "../../menu";
 import Menu from "../Main/Menu";
 const Discover = () => {
@@ -6,11 +7,13 @@ const Discover = () => {
     return <Menu key={el.id} element={el} />;
   });
   return (
-    <section className={style.discover__container}>
-      <h3 className={style.discover__heading}>Discover</h3>
-      <h2 className={style.discover__main}>Our Food Menu</h2>
-      <div className={style.discover__menu}>{menuData}</div>
-    </section>
+    <Bounce bottom>
+      <section className={style.discover__container}>
+        <h3 className={style.discover__heading}>Discover</h3>
+        <h2 className={style.discover__main}>Our Food Menu</h2>
+        <div className={style.discover__menu}>{menuData}</div>
+      </section>
+    </Bounce>
   );
 };
 
