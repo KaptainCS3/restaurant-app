7a885e8c84064b981b5d1ccb0fe149a579c649d8
KaptainCS3 <mbiapplegryn@gmail.com>
2023-01-07 20:15:36 +0100

    fix Card bug

--- src/components/Main/CardContainer.js
+++ src/components/Main/CardContainer.js
@@ -5,11 +5,11 @@ import Card from "./Card";
 // import Paging from "./Paging";
 const CardContainer = () => {
   const [width, setWidth] = useState(0);
-  // const carousel = useRef();
+  const carousel = useRef();
 
-  // useEffect(() => {
-  //   setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
-  // }, []);
+  useEffect(() => {
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
 
   return (
     <section className={style.feature__food}>
@@ -20,18 +20,60 @@ const CardContainer = () => {
           {/* <Paging /> */}
         </div>
       </div>
+      <motion.div className={style.carousel} ref={carousel}>
+        <motion.div
+          className={style.inner__carousel}
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+        >
+          <motion.div
+            className="flex flex-row justify-between"
+            style={{ paddingLeft: "5%" }}
+          >
+            <Card img="image1.jpg" />
+            <Card img="image2.jpg" />
+            <Card img="image3.jpg" />
+            <Card img="image4.jpg" />
+            <Card img="image5.jpg" />
+            <Card img="image6.jpg" />
+            <Card img="image7.jpg" />
+            <Card img="image8.jpg" />
+            <Card img="image9.jpg" />
+            <Card img="image10.jpg" />
+            {/* <Card img="image11.jpg" /> */}
+            {/* <Card img="image12.jpg" /> */}
+            {/* <Card img="image13.jpg" /> */}
+            {/* <Card img="image14.jpg" /> */}
+            {/* <Card img="image15.jpg" /> */}
+            {/* <Card img="image16.jpg" /> */}
+            {/* <Card img="image17.jpg" /> */}
+          </motion.div>
+        </motion.div>
+      </motion.div>
+      {/* <div className={style.carousel}>
+  
+          <Card img="pexels-steve-3789885.jpg"/>
+          <Card img="pexels-steve-3789885.jpg"/>
+          <Card img="pexels-steve-3789885.jpg"/>
+          <Card img="pexels-steve-3789885.jpg"/>
+          <Card img="pexels-steve-3789885.jpg"/>
+          <Card img="pexels-steve-3789885.jpg"/>
+          <Card img="pexels-steve-3789885.jpg"/>
+          <Card img="pexels-steve-3789885.jpg"/>
+          <Card img="pexels-steve-3789885.jpg"/>
+          <Card img="pexels-steve-3789885.jpg"/>
+        </div>
+        <div
+          className="flex flex-row "
+          style={{ padding: "2% 5%" }}
+        >
+          <Card img="pexels-nishant-aneja-2955819.jpg" />
+          <Card img="pexels-rajesh-tp-1633578.jpg" />
+          {/* <Card img="pexels-steve-3789885.jpg"/> */}
+      {/* </div> */}
+      {/* </div> */}
+      {/* <Card img="pexels-rajesh-tp-1633578.jpg"/> */}
     </section>
   );
 };
-
-{/* <Card img="image1.jpg" />
-<Card img="image2.jpg" />
-<Card img="image3.jpg" />
-<Card img="image4.jpg" />
-<Card img="image5.jpg" />
-<Card img="image6.jpg" />
-<Card img="image7.jpg" />
-<Card img="image8.jpg" />
-<Card img="image9.jpg" />
-<Card img="image10.jpg" /> */}
 export default CardContainer;
