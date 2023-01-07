377cc2fe2476ffa13bc7d9b69b1216e987ba80e5
KaptainCS3 <mbiapplegryn@gmail.com>
2023-01-07 20:15:30 +0100

    fix Card bug

--- src/components/Main/Card.js
+++ src/components/Main/Card.js
@@ -1,19 +1,19 @@
  const Card = (props) => {
   const card = {
-    marginRight: 50,
-    marginTop: 80,
-    minWidth: "10%"
-  }
+    marginRight: 40,
+    marginTop: "7%",
+    minWidth: "35%",
+  };
   // const cartArray = ["pexels-nishant-aneja-2955819.jpg","pexels-rajesh-tp-1633578.jpg"];
   return (
-    <div class="flex justify-evenly" style={card}>
-      <div class="flex flex-rows md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+    <div class="flex justify-center" style={card}>
+      <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-2xl">
         <img
           class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
           src={require(`../../assets/food_features/${props.img}`)}
           alt=""
         />
-        <div class="p-6 flex flex-col justify-evenly">
+        <div class="p-6 flex flex-col justify-start">
           <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
           <p class="text-gray-700 text-base mb-4">
             This is a wider card with supporting text below as a natural lead-in
@@ -25,5 +25,4 @@
     </div>
   );
 };
-
 export default Card;
