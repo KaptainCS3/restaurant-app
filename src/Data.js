1d00639d8a7a575866e0190156a37ffeffd5865e
KaptainCS3 <mbiapplegryn@gmail.com>
2023-01-08 01:17:27 +0100

    insert data form testimonial

--- src/Data.js
+++ src/Data.js
@@ -1,49 +1,36 @@
-
-export default [
+const people = [
   {
     id: 1,
-    "dish_image": "/pexels-pixabay-56014.jpg",
-    "dish_name": "salad",
-    // "rating_stars": "./"
-    rating: 4,
-    order_now: true,
+    name: "susan smith",
+    job: "web developer",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nihil fugiat inventore autem sapiente? Odit quam expedita vero quisquam laboriosam hic sit, voluptas dolorum harum quis officia placeat adipisci vel non mollitia ipsam inventore? Laudantium et quasi cupiditate tempora minima fugiat sit ea, deserunt, adipisci deleniti eos sequi voluptatem cumque?",
   },
   {
     id: 2,
-    "dish_image": "/pexels-julie-aagaard-2097090.jpg",
-    "dish_name": "salad",
-    // "rating_stars": "./"
-    rating: 4,
-    order_now: true,
+    name: "anna johnson",
+    job: "web designer",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci suscipit consequatur cupiditate provident, maxime eaque a dolorum sit fuga placeat maiores est. Repellat corrupti voluptatum assumenda ad vero voluptate dolorem modi tenetur. Ea, non veniam, repellendus facere labore debitis corrupti est consequatur molestias quisquam accusantium perferendis tempora veritatis iusto error?",
   },
   {
     id: 3,
-    dish_image: "/pexels-cats-coming-406152.jpg",
-    dish_name: "salad",
-    // "rating_stars": "./"
-    rating: 4,
-    order_now: true,
+    name: "peter jones",
+    job: "intern",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta, facilis? Provident consequatur doloremque tempore ea inventore. Recusandae quibusdam eos veniam et nobis molestiae libero neque itaque, saepe corrupti porro iure voluptatem blanditiis maxime inventore amet dolorum harum quos labore alias ipsum nostrum? Doloribus deleniti ea quos excepturi temporibus magnam harum?",
   },
   {
     id: 4,
-    dish_image: "/pexels-iina-luoto-1211887.jpg",
-    dish_name: "salad",
-    // "rating_stars": "./"
-    rating: 4,
-    order_now: true,
+    name: "bill anderson",
+    job: "the boss",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id blanditiis porro ex, placeat officia asperiores eius quia quibusdam numquam aliquam pariatur corrupti eveniet cum suscipit nam nobis similique itaque accusantium, ratione beatae. Ad facilis id alias, beatae unde deserunt eum numquam, neque explicabo ut voluptatum vero cumque tenetur sapiente voluptas?",
   },
-  // {
-  //   id: 5,
-  //   dish_image: "/pexels-iina-luoto-1211887.jpg",
-  //   dish_name: "salade",
-  //   rating: 4,
-  //   "order-now": true,
-  // },
-  // {
-  //   id: 6,
-  //   dish_image: "/pexels-iina-luoto-1211887.jpg",
-  //   dish_name: "salade",
-  //   rating: 4,
-  //   "order-now": true,
-  // },
 ];
+
+export default people;
