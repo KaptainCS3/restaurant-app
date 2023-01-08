d4e96e89cd4e050bd3244d4388d58e252d47bad0
KaptainCS3 <mbiapplegryn@gmail.com>
2023-01-08 01:22:17 +0100

    paste testimonial component code

--- src/components/Main/CustomerCard.js
+++ src/components/Main/CustomerCard.js
@@ -1,23 +1,84 @@
-import React from 'react'
+import React, { useState } from "react";
+import people from "../../data";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 import style from "../../style/customerCard.module.css"
-import avarta_1 from "../../assets/avartas/pexels-daniel-xavier-1239291.jpg"
-import avarta_2 from "../../assets/avartas/pexels-pixabay-220453.jpg"
-import avarta_3 from "../../assets/avartas/pexels-stefan-stefancik-91227.jpg"
-const CustomerCard = () => {
-  return (
-    <div className={style.card__customer}>
-      <div className={style.avarta__rate}>
-        <img src={avarta_1} alt="pexel" />
-        <div className={style.rating}>adfafdslfsidf</div>
-      </div>
-      <p className={style.description}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-        voluptate nemo porro voluptas tempore. Est incidunt doloribus ea esse,
-        alias excepturi beatae dolore laboriosam modi quam aperiam aliquam quia
-        temporibus?.
-      </p>
-    </div>
-  );
-}
 
-export default CustomerCard
+const CustomerCard = () => {
+  // The review is same as a carousel you know in css and normal js
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  // This function helps us check and make sure we dont go above the lenght of our array and below its lenght
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) return people.length - 1;
+    return number;
+  };
+
+  // This function help us in moving to the previous element
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      // if(newIndex < 0)
+      // newIndex = 3
+      // checkNumber(newIndex)
+      return checkNumber(newIndex);
+    });
+  };
+
+  // This function help us in moving to the next element
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      // if(newIndex > people.length -1)
+      // newIndex = 0
+      // checkNumber(newIndex)
+      return checkNumber(newIndex);
+    });
+  };
+
+  // in this function , we use the random object to generate random numbers so we can get random images within our range
+  const randomPerson = () => {
+    let randomIndex = Math.floor(Math.random() * people.length);
+    if (randomIndex === index) {
+      randomIndex = index + 1;
+    }
+    setIndex(checkNumber(randomIndex));
+    // return randomIndex
+  };
+  return (
+    <>
+      <article className={style.review}>
+        <div className={style.img__container}>
+          <img src={image} alt={name} className={style.person__img} />
+          <span className={style.quote__icon}>
+            <FontAwesomeIcon icon={faChevronRight} />
+          </span>
+        </div>
+        <h4 className={style.author}>{name}</h4>
+        <p className={style.job}>{job}</p>
+        <p className={style.info}>{text}</p>
+        <div className={style.button__container}>
+          <button className={style.prev__btn} onClick={prevPerson}>
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </button>
+          <button className={style.random__btn} onClick={randomPerson}>
+            suprise me
+          </button>
+          <button className={style.next__btn} onClick={nextPerson}>
+            <FontAwesomeIcon icon={faChevronRight} />
+          </button>
+        </div>
+      </article>
+    </>
+  );
+};
+
+
+  export default CustomerCard
